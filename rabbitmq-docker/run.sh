3070ac5953ed836c82180272bfef5a23c252213d
Niklas Renner Nielsen <niklasrennernielsen@gmail.com>
2017-11-26 20:16:57 +0100

    fixed spelling mistake

--- rabbitmq-docker/run.sh
+++ rabbitmq-docker/run.sh
@@ -2,7 +2,7 @@
 
 rabbitmq_name="rabbitmq-rpc"
 
-docker kill $rabitmq_name
+docker stop $rabbitmq_name
 docker rm $rabbitmq_name
 docker build -t $rabbitmq_name .
 docker run -d --name $rabbitmq_name -p 15672:15672 -p 5672:5672 $rabbitmq_name
